13fff132bd66459aa9d1900039645dc9aaf3e9dc
Mark D. Thompson <thomesoni@gmail.com>
2019-01-13 17:41:49 -0500

    blocked out marketplace

--- client/src/components/marketplace/Marketplace.js
+++ client/src/components/marketplace/Marketplace.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
 import ListProducts from "./ListProducts";
 import ListOrders from "./ListOrders";
+import { Row, Col } from 'reactstrap';
 
 export default class Marketplace extends Component{
+   
     render() {
+               
         return(
-            <div className="marketplace">
-                
-                <ListProducts />
-
-                <ListOrders />
-                
-            </div>
+            <Row id="administration">
+                <Col>
+ 
+                    <Row>
+                        <Col>
+                            <ListProducts drizzle={this.props.drizzle} drizzleState={this.props.drizzleState} />
+                        </Col>
+                    </Row>
+                    
+                    <Row>
+                        <Col>
+                            <ListOrders drizzle={this.props.drizzle} drizzleState={this.props.drizzleState} />
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
         )
+
     }
 }
--- client/src/components/shop/ListShops.js
+++ client/src/components/shop/ListShops.js
@@ -3,17 +3,67 @@ import { Button, Table } from 'reactstrap';
 
 export default class ListShops extends Component{
 
- render(){
+    constructor(props){
+        super(props);
+        this.state = { shopIDs: null };
+    }
+    
+    componentDidMount() {
+    
+        const { drizzle, drizzleState } = this.props;
+        const contract = drizzle.contracts.Marketplace;
+        const account = drizzleState.accounts[0];
+    
+        // let drizzle know we want to watch the `get` method
+        const shopIDs = contract.methods["getShopIDsByOwner"].cacheCall(account, {from: account});
+    
+        // save the `dataKey` to local component state for later reference
+        this.setState({ shopIDs });
+    }
+
+    render() {
+
+        // get the contract state from drizzleState
+        const { Marketplace } = this.props.drizzleState.contracts;
+
+        // using the saved `dataKey`, get the variable we're interested in
+        const myIDs = Marketplace.getShopIDsByOwner[this.state.shopIDs];
+
+        const l = (myIDs && myIDs.value);
+
+        console.log(l);
+        /*
+        try{
+           
+            if(l.length !== undefined){
+
+                const adminList = l.map((admin, index) =>
+                    <tr key={index}><td>{admin}</td><td>
+                        {
+                           (index < 1) ? <Button type="button" disabled>Owner</Button>
+                                        : <Button type="button" onClick={this.removeAdmin.bind(this, index)}>Remove</Button>
+                        }   
+                    </td></tr>
+                )
+            
+                return (
+                    <div>
+                        <h3>Admin Accounts</h3>
+                        <Table size="sm" striped>
+                            <thead><tr><th>Account Address</th><th>Action</th></tr></thead>
+                            <tbody>{adminList}</tbody>
+                        </Table>
+                    </div>
+                )
+            }    
+
+        } catch(err){
+           
+        }
+        */
+
         return(
-            <div>
-            <h3>List Shops</h3>
-            <table>
-                <thead><tr><th>Address</th><th>ID</th><th>Name</th><th>Category</th><th>Action</th></tr></thead>
-                <tbody>
-                    <tr><td>0x000000000000000000000000</td><td>0</td><td>Store 00</td><td></td><td><Button type="submit">Delete</Button></td></tr>
-                </tbody>
-            </table>
-            </div>
+            <div><p>No shops.</p></div>
         );
     }
 }
