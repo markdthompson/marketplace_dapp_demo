426f63677bc693f0cb31d75f9839f2638bc3a9ed
Mark D. Thompson <thomesoni@gmail.com>
2019-01-24 08:41:40 -0500

    Updates

---
+++ precache-manifest.25436cab67b02a7f2048cbb01b977548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "50e4bfc1b7e2e385086a",
+    "url": "/markdthompson/marketplace_dapp_demo/static/js/runtime~main.50e4bfc1.js"
+  },
+  {
+    "revision": "f4d1b94b8c73bc5bcfc6",
+    "url": "/markdthompson/marketplace_dapp_demo/static/js/main.f4d1b94b.chunk.js"
+  },
+  {
+    "revision": "db74fd6245b47df40949",
+    "url": "/markdthompson/marketplace_dapp_demo/static/js/1.db74fd62.chunk.js"
+  },
+  {
+    "revision": "f4d1b94b8c73bc5bcfc6",
+    "url": "/markdthompson/marketplace_dapp_demo/static/css/main.1c18c46d.chunk.css"
+  },
+  {
+    "revision": "db74fd6245b47df40949",
+    "url": "/markdthompson/marketplace_dapp_demo/static/css/1.110198c5.chunk.css"
+  },
+  {
+    "revision": "7f112380c2d1fe4bc0b4a4b4780f235a",
+    "url": "/markdthompson/marketplace_dapp_demo/index.html"
+  }
+];
