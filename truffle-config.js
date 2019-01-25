1446e40ae3991854671e736ae56cf83dbcda0273
Mark D. Thompson <thomesoni@gmail.com>
2019-01-25 14:35:55 -0500

    bumped up gas for dev

--- truffle-config.js
+++ truffle-config.js
@@ -23,7 +23,7 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*", // Match any network id
-      gas: 6000000,
+      gas: 6500000,
       gaslimit: 10000000000
     },
     ropsten: {
