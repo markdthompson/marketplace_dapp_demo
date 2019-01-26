4138521f97deba1e5937e424010e082587c151b5
Mark D. Thompson <thomesoni@gmail.com>
2019-01-25 19:31:17 -0500

    updated landing page initial walkthru test

--- client/src/components/marketplace/MarketplaceContainer.js
+++ client/src/components/marketplace/MarketplaceContainer.js
@@ -55,14 +55,14 @@ export default class MarketplaceContainer extends Component{
                         <li>Make sure you're logged into MetaMask as Account 1</li>
                         <li>Click the 'Admin' link to the top left of the header</li>
                         <li>Look to the bottom of the Admin page for the 'Add ShopOwner' section</li>
-                        <li>In MetaMask, copy another account address, and then paste it into the 'Enter Account Address' field</li>
+                        <li>In MetaMask, copy another account address, or use your Account 1 address, and paste it into the 'Enter Account Address' field</li>
                         <li>Click the 'Add' Button</li>
                         <li>MetaMask will prompt you to confirm the transaction, confirm it</li>
                     </ol>
 
                     <h2>II. Open a Shop</h2>
                     <ol>
-                        <li>In MetaMask, switch to the account you added to the ShopOwners list in step I</li>
+                        <li>In MetaMask, switch to the account you added to the ShopOwners list in step I (if you gave your Account 1 ShopOwner permissions skip to step 4) </li>
                         <li>Reload the app to load the new account by clicking on the 'Marketplace Dapp Demo' link in the header</li>
                         <li>Check the Active External Account field in the header to make sure you're logged in with the ShopOwner account</li>
                         <li>Click the 'Manage Shops' link to the top left of the header</li>
